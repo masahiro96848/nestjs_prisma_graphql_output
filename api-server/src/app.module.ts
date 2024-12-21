6bd01e31ca345aaec7619202e864287641b8798e
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-21 09:01:56 +0900

    graphqlを整理

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { TaskModule } from './task/task.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,13 +11,12 @@ import { TaskModule } from './task/task.module';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
-      sortSchema: true,
     }),
     TaskModule,
-    // ConfigModule.forRoot({
-    //   isGlobal: true, // ConfigModuleをグローバルにすることで、他のモジュールで再インポート不要
-    //   envFilePath: '.env', // デフォルトで'.env'を読み込むので省略可能
-    // }),
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModuleをグローバルにすることで、他のモジュールで再インポート不要
+      envFilePath: '.env', // デフォルトで'.env'を読み込むので省略可能
+    }),
   ],
 })
 export class AppModule {}
