c5015c99e697485726e1650591dbba81cb1e81cd
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-22 10:00:59 +0900

    ユーザー取得クエリを作成

--- api-server/src/user/user.service.ts
+++ api-server/src/user/user.service.ts
@@ -19,4 +19,12 @@ export class UserService {
       },
     });
   }
+
+  async getUser(email: string): Promise<User> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
 }
